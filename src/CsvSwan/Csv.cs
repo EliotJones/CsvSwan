a2c82954e3d8df4f4f9d0df174819e3091a21c40
Eliot Jones <elioty@hotmail.co.uk>
2020-02-12 17:13:34 +0000

    store header row once
    rather than evaluate the header row getter every time we only read it once. this prevents weird stream resetting when evaluating the header row in the debugger.
    
    move the type map factory to a new file and add support for long and short numerics.

--- src/CsvSwan/Csv.cs
+++ src/CsvSwan/Csv.cs
@@ -4,8 +4,6 @@
     using System.Collections.Generic;
     using System.Globalization;
     using System.IO;
-    using System.Linq;
-    using System.Reflection;
     using System.Text;
 
     /// <inheritdoc />
@@ -18,6 +16,7 @@
         #region Static Factories
 
         public static Csv Open(string filename, char separator = ',') => new Csv(File.OpenRead(filename), CsvOptions.WithSeparator(separator), true);
+        public static Csv Open(string filename, CsvOptions options) => new Csv(File.OpenRead(filename), options, true);
         public static Csv Open(Stream fileStream, char separator = ',') => new Csv(fileStream, CsvOptions.WithSeparator(separator), false);
         public static Csv Open(byte[] fileBytes, char separator = ',') => new Csv(new MemoryStream(fileBytes), CsvOptions.WithSeparator(separator), true);
         public static Csv Open(Stream fileStream, CsvOptions options) => new Csv(fileStream, options, false);
@@ -49,7 +48,7 @@
         /// Get the header row values for this file. If there is no header row (<see cref="CsvOptions.HasHeaderRow"/> is
         /// <see langword="false"/>) then this is an empty list.
         /// </summary>
-        public IReadOnlyList<string> HeaderRow => reader.GetHeaderRow();
+        public IReadOnlyList<string> HeaderRow { get; }
 
         /// <summary>
         /// Step through the rows in this CSV. <see cref="RowAccessor"/> should not be stored.
@@ -96,6 +95,15 @@
 
             reader = new CsvReader(this.stream, options);
             accessor = new RowAccessor(this);
+
+            if (options.HasHeaderRow)
+            {
+                HeaderRow = reader.GetHeaderRow();
+            }
+            else
+            {
+                HeaderRow = Array.Empty<string>();
+            }
         }
 
         public IReadOnlyList<IReadOnlyList<string>> GetAllRowValues()
@@ -109,8 +117,6 @@
 
             return result;
         }
-
-
 
         public IEnumerable<T> MapRows<T>()
         {
@@ -142,8 +148,6 @@
             private readonly object mutex = new object();
             private readonly Dictionary<Type, TypeMapFactory> typeFactories = new Dictionary<Type, TypeMapFactory>();
 
-            private readonly object[] setterBuffer = new object[1];
-
             internal RowAccessor(Csv csv)
             {
                 this.csv = csv ?? throw new ArgumentNullException(nameof(csv));
@@ -158,28 +162,34 @@
                 return new List<string>(csv.currentValues);
             }
 
+            public short GetShort(int index, IFormatProvider formatProvider = null)
+            {
+                GuardIndex(index);
+                return short.Parse(csv.currentValues[index], NumberStyles.Number, formatProvider ?? CultureInfo.InvariantCulture);
+            }
+
             public int GetInt(int index, IFormatProvider formatProvider = null)
             {
                 GuardIndex(index);
-                return int.Parse(csv.currentValues[index], NumberStyles.Number, formatProvider ?? CultureInfo.CurrentCulture);
+                return int.Parse(csv.currentValues[index], NumberStyles.Number, formatProvider ?? CultureInfo.InvariantCulture);
             }
 
             public long GetLong(int index, IFormatProvider formatProvider = null)
             {
                 GuardIndex(index);
-                return long.Parse(csv.currentValues[index], NumberStyles.Number, formatProvider ?? CultureInfo.CurrentCulture);
+                return long.Parse(csv.currentValues[index], NumberStyles.Number, formatProvider ?? CultureInfo.InvariantCulture);
             }
 
             public decimal GetDecimal(int index, IFormatProvider formatProvider = null)
             {
                 GuardIndex(index);
-                return decimal.Parse(csv.currentValues[index], NumberStyles.Number, formatProvider ?? CultureInfo.CurrentCulture);
+                return decimal.Parse(csv.currentValues[index], NumberStyles.Number, formatProvider ?? CultureInfo.InvariantCulture);
             }
 
             public double GetDouble(int index, IFormatProvider formatProvider = null)
             {
                 GuardIndex(index);
-                return double.Parse(csv.currentValues[index], NumberStyles.Number, formatProvider ?? CultureInfo.CurrentCulture);
+                return double.Parse(csv.currentValues[index], NumberStyles.Number, formatProvider ?? CultureInfo.InvariantCulture);
             }
 
             public string GetString(int index)
@@ -211,97 +221,7 @@
                         typeFactories[typeof(T)] = factory;
                     }
 
-                    return (T) factory.Map(this, formatProvider);
-                }
-            }
-
-            internal class TypeMapFactory
-            {
-                private readonly object[] setterBuffer = new object[1];
-
-                private readonly Type type;
-                private readonly IReadOnlyList<(int column, MethodInfo setter, Type propertyType)> propertySetters;
-
-                private TypeMapFactory(Type type, IReadOnlyList<(int column, MethodInfo setter, Type propertyType)> propertySetters)
-                {
-                    this.type = type ?? throw new ArgumentNullException(nameof(type));
-                    this.propertySetters = propertySetters ?? throw new ArgumentNullException(nameof(propertySetters));
-                }
-
-                public static TypeMapFactory Create<T>(IReadOnlyList<string> columnHeaders = null)
-                {
-                    // TODO: column headers.
-
-                    var type = typeof(T);
-                    var props = type.GetProperties();
-
-                    var setters = new List<(int index, MethodInfo setter, Type propertyType)>();
-                    var settersUnmapped = new List<(MethodInfo, Type propertyType)>();
-
-                    foreach (var prop in props)
-                    {
-                        var attr = prop.GetCustomAttribute<CsvColumnOrderAttribute>();
-
-                        if (attr == null)
-                        {
-                            settersUnmapped.Add((prop.GetSetMethod(true), prop.PropertyType));
-                            continue;
-                        }
-
-                        var val = attr.ColumnIndex;
-
-                        setters.Add((val, prop.GetSetMethod(true), prop.PropertyType));
-                    }
-
-                    if (setters.Count > 0)
-                    {
-                        return new TypeMapFactory(type, setters);
-                    }
-
-                    return new TypeMapFactory(type, settersUnmapped.Select((x, i) => (i, x.Item1, x.propertyType)).ToList());
-                }
-
-                public object Map(RowAccessor row, IFormatProvider formatProvider)
-                {
-                    var instance = Activator.CreateInstance(type);
-
-                    foreach (var setter in propertySetters)
-                    {
-                        var propertyType = setter.propertyType;
-
-                        object value;
-
-                        if (propertyType == typeof(string))
-                        {
-                            value = row.GetString(setter.column);
-                        }
-                        else if (propertyType == typeof(int))
-                        {
-                            value = row.GetInt(setter.column, formatProvider);
-                        }
-                        else if (propertyType == typeof(double))
-                        {
-                            value = row.GetDouble(setter.column, formatProvider);
-                        }
-                        else if (propertyType == typeof(float))
-                        {
-                            value = (float) row.GetDouble(setter.column, formatProvider);
-                        }
-                        else if (propertyType == typeof(decimal))
-                        {
-                            value = row.GetDecimal(setter.column, formatProvider);
-                        }
-                        else
-                        {
-                            throw new InvalidOperationException($"Cannot map to type {propertyType.FullName} on type {type.FullName}.");
-                        }
-
-                        setterBuffer[0] = value;
-
-                        setter.setter.Invoke(instance, setterBuffer);
-                    }
-
-                    return instance;
+                    return (T)factory.Map(this, formatProvider);
                 }
             }
         }
---
+++ src/CsvSwan/TypeMapFactory.cs
@@ -0,0 +1,105 @@
+﻿namespace CsvSwan
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    internal class TypeMapFactory
+    {
+        private readonly object[] setterBuffer = new object[1];
+
+        private readonly Type type;
+        private readonly IReadOnlyList<(int column, MethodInfo setter, Type propertyType)> propertySetters;
+
+        private TypeMapFactory(Type type, IReadOnlyList<(int column, MethodInfo setter, Type propertyType)> propertySetters)
+        {
+            this.type = type ?? throw new ArgumentNullException(nameof(type));
+            this.propertySetters = propertySetters ?? throw new ArgumentNullException(nameof(propertySetters));
+        }
+
+        public static TypeMapFactory Create<T>(IReadOnlyList<string> columnHeaders = null)
+        {
+            // TODO: column headers.
+
+            var type = typeof(T);
+            var props = type.GetProperties();
+
+            var setters = new List<(int index, MethodInfo setter, Type propertyType)>();
+            var settersUnmapped = new List<(MethodInfo, Type propertyType)>();
+
+            foreach (var prop in props)
+            {
+                var attr = prop.GetCustomAttribute<CsvColumnOrderAttribute>();
+
+                if (attr == null)
+                {
+                    settersUnmapped.Add((prop.GetSetMethod(true), prop.PropertyType));
+                    continue;
+                }
+
+                var val = attr.ColumnIndex;
+
+                setters.Add((val, prop.GetSetMethod(true), prop.PropertyType));
+            }
+
+            if (setters.Count > 0)
+            {
+                return new TypeMapFactory(type, setters);
+            }
+
+            return new TypeMapFactory(type, settersUnmapped.Select((x, i) => (i, x.Item1, x.propertyType)).ToList());
+        }
+
+        public object Map(Csv.RowAccessor row, IFormatProvider formatProvider)
+        {
+            var instance = Activator.CreateInstance(type);
+
+            foreach (var setter in propertySetters)
+            {
+                var propertyType = setter.propertyType;
+
+                object value;
+
+                if (propertyType == typeof(string))
+                {
+                    value = row.GetString(setter.column);
+                }
+                else if (propertyType == typeof(short))
+                {
+                    value = row.GetShort(setter.column, formatProvider);
+                }
+                else if (propertyType == typeof(int))
+                {
+                    value = row.GetInt(setter.column, formatProvider);
+                }
+                else if (propertyType == typeof(long))
+                {
+                    value = row.GetLong(setter.column, formatProvider);
+                }
+                else if (propertyType == typeof(double))
+                {
+                    value = row.GetDouble(setter.column, formatProvider);
+                }
+                else if (propertyType == typeof(float))
+                {
+                    value = (float) row.GetDouble(setter.column, formatProvider);
+                }
+                else if (propertyType == typeof(decimal))
+                {
+                    value = row.GetDecimal(setter.column, formatProvider);
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Cannot map to type {propertyType.FullName} on type {type.FullName}.");
+                }
+
+                setterBuffer[0] = value;
+
+                setter.setter.Invoke(instance, setterBuffer);
+            }
+
+            return instance;
+        }
+    }
+}
